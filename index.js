a0eeac62cdb513ff675dfabc52f7e4c8fd3e5326
tanviway48 <tanvi.waykar04@gmail.com>
2024-07-13 14:01:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function imgslider(anything){
+  document.querySelector('.shoe').src=anything;
+}
+function changebgcolor(color){
+  const one=document.querySelector('.one');
+  one.style.background=color;
+}
